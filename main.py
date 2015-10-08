93b66299b05991e9aff8ed9465ea66d7c307ccf9
Alexey Nabrodov <alexey.nabrodov@dev.zodiac.tv>
2015-10-08 19:29:58 +0300

    languages

--- main.py
+++ main.py
@@ -6,55 +6,123 @@ import os
 import sys
 import dateutil.parser
 from creds import login, token
+import json
+from datetime import datetime, timedelta
 CWD = os.path.abspath(os.path.split(sys.argv[0])[0])
 
 user = sys.argv[1]
 
-# TODO: fetch_list func (per page)
+
+def td_format(td_object):
+    seconds = int(td_object.total_seconds())
+    periods = [
+        ('year',        60 * 60 * 24 * 365),
+        ('month',       60 * 60 * 24 * 30),
+        ('day',         60 * 60 * 24),
+        # ('hour',        60 * 60),
+        # ('minute',      60),
+        # ('second',      1)
+    ]
+
+    strings = []
+    for period_name, period_seconds in periods:
+        if seconds > period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            if period_value == 1:
+                strings.append("%s %s" % (period_value, period_name))
+            else:
+                strings.append("%s %ss" % (period_value, period_name))
+
+    return ", ".join(strings)
+
+def fetch(_url, paginate=False, getter=None):
+    print('Fetching: ' + _url)
+    p = 1
+    if paginate:
+        url = _url % p
+    else:
+        url = _url
+    items = requests.get(url, auth=HTTPBasicAuth(login, token)).json()
+    if getter is not None:
+        items = getter(items)
+    if paginate and len(items) == 100:
+        p += 1
+        new_items = requests.get(_url % p, auth=HTTPBasicAuth(login, token)).json()
+        items.extend(new_items)
+        while new_items:
+            p += 1
+            new_items = requests.get(_url % p, auth=HTTPBasicAuth(login, token)).json()
+            items.extend(new_items)
+    return items
+
 # TODO: repos/pr lang and value analysis
 
-info = requests.get('https://api.github.com/users/%s' %
-                    user, auth=HTTPBasicAuth(login, token)).json()
-events = requests.get(info['events_url'].replace(
-    '{/privacy}', ''), auth=HTTPBasicAuth(login, token)).json()
-stars = requests.get(info['starred_url'].replace(
-    '{/owner}{/repo}', '?page=1&per_page=1000'), auth=HTTPBasicAuth(login, token)).json()
-_issues = requests.get('https://api.github.com/search/issues?q=author:%s&page=1&per_page=1000' %
-                       user, auth=HTTPBasicAuth(login, token)).json()
-issues = [x for x in _issues['items'] if user not in x['url'] and 'pull_request' not in x]
-pulls = [x for x in _issues['items'] if 'pull_request' in x]
+info = fetch('https://api.github.com/users/%s' % user)
+_issues = fetch('https://api.github.com/search/issues?q=author:%s&page=%%s&per_page=100' % user, True, lambda x: x['items'])
+issues = [x for x in _issues if user not in x[
+    'url'] and 'pull_request' not in x]
+pulls = [x for x in _issues if 'pull_request' in x]
 for p in pulls:
-    p['info'] = requests.get(
-        p['pull_request']['url'] + '?page=1&per_page=1000',
-        auth=HTTPBasicAuth(login, token)
-    ).json()
+    p['info'] = fetch(p['pull_request']['url'])
 
-_repos = requests.get(
-    info['repos_url'] + '?page=1&per_page=1000', auth=HTTPBasicAuth(login, token)).json()
+_repos = fetch(info['repos_url'] + '?page=%s&per_page=1000', True)
 repos = {
     'items': _repos,
     'forks': len([x for x in _repos if x['fork']]),
-    'pulls': []
+    'pulls': [],
+    '_languages': {},
+    '_pulls_languages': {},
 }
-repos['pulls'].extend(pulls)
-repos['pulls_merged'] = len([x for x in repos['pulls'] if x['info']['merged_at'] is not None])
+for repo in _repos:
+    l = repo['language']
+    if l is None:
+        l = 'Unknown'
+    if l not in repos['_languages']:
+        repos['_languages'][l] = [0, 0]
+    repos['_languages'][l][0] += 1
+    repos['_languages'][l][1] = '%s%%' % int(repos['_languages'][l][0] / len(_repos) * 100)
+repos['languages'] = sorted(repos['_languages'].items(), key=lambda x: x[1][0], reverse=True)
 
+repos['pulls'].extend(pulls)
+repos['pulls_merged'] = len(
+    [x for x in repos['pulls'] if x['info']['merged_at'] is not None])
+for pr in repos['pulls']:
+  #print(json.dumps(pr, indent=4))
+    l = pr['info']['base']['repo']['language']
+    if l is None:
+        l = 'Unknown'
+    if l not in repos['_pulls_languages']:
+        repos['_pulls_languages'][l] = [0, 0]
+    repos['_pulls_languages'][l][0] += 1
+    repos['_pulls_languages'][l][1] = '%s%%' % int(repos['_pulls_languages'][l][0] / len(_repos) * 100)
+repos['pulls_languages'] = sorted(repos['_pulls_languages'].items(), key=lambda x: x[1][0], reverse=True)
+
+events = fetch(info['events_url'].replace('{/privacy}', ''))
 info['last_activity'] = dateutil.parser.parse(
     [x for x in events if x['actor']['login'] == user][0]['created_at']
 ).strftime('%d.%m.%Y')
+info['duration'] = td_format(datetime.now() - dateutil.parser.parse(
+    info['created_at']
+).replace(tzinfo=None))
 info['created_at'] = dateutil.parser.parse(
     info['created_at']
 ).strftime('%d.%m.%Y')
 
-
+stars = fetch(info['starred_url'].replace('{/owner}{/repo}', '?page=%s&per_page=100'), True)
 template = jinja2.Template(open(os.path.join(CWD, 'report.tpl'), 'r').read())
-report = template.render({
-    "user": info,
+print('Rendering')
+context = {
+    'user': info,
     'repos': repos,
     'issues': issues,
-    'stars': stars
-})
+    'stars': stars,
+    'timestamp': datetime.now()
+}
+report = template.render(context)
 
 report_file = 'report.html'
+
 with open(report_file, 'w') as f:
     f.write(report)
+#context['timestamp'] = ''
+#json.dump(context, open('info.json', 'w'))
--- report.tpl
+++ report.tpl
@@ -1,19 +1,48 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Github profile: {{user.name}} [{{user.login}}]</title>
-  </head>
-  <body>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.5/material.indigo-pink.min.css">
+  <script src="https://storage.googleapis.com/code.getmdl.io/1.0.5/material.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <title>Github profile: {{user.name}} [{{user.login}}]</title>
+</head>
+
+<body>
+  <div class="" style="width: 400px; margin: 40px auto;">
     <div class="github-card" data-github="{{user.name}}" data-width="400" data-height="316" data-theme="medium"></div>
-    <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
-    <ul>
-      <li>Joined: {{user.created_at}}</li>
-      <li>Last activity: {{user.last_activity}}</li>
-      <li>Repos: {{user.public_repos}} (Forks: {{repos.forks}})</li>
-      <li>Pull requests: {{repos.pulls|length}} (Merged: {{repos.pulls_merged}})</li>
-      <li>Issues in foregin repos: {{issues|length}}</li>
-      <li>Stars: {{stars|length}}</li>
-    </ul>
-  </body>
+    <script src="https://cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+    <div class="demo-card-wide mdl-card mdl-shadow--2dp" style="width: 400px; margin-top: 20px">
+      <div class="mdl-card__title">
+        <h3 class="mdl-card__title-text">Github Info</h3>
+      </div>
+      <div class="mdl-card__supporting-text">
+        <ul>
+          <li>Joined: {{user.created_at}} ({{user.duration}})</li>
+          <li>Last activity: {{user.last_activity}}</li>
+          <li>Repos: {{user.public_repos}} (Forks: {{repos.forks}})</li>
+          <li>Repos Languages:<ul>
+            {% for l in repos.languages %}
+            <li>{{l[0]}}: {{l[1][0]}} repos ({{l[1][1]}})</li>
+            {%endfor%}
+          </ul></li>
+          <li>Pull requests: {{repos.pulls|length}} (Merged: {{repos.pulls_merged}})</li>
+          <li>Pulls Languages:<ul>
+            {% for l in repos.pulls_languages %}
+            <li>{{l[0]}}: {{l[1][0]}} repos ({{l[1][1]}})</li>
+            {%endfor%}
+          </ul></li>
+          <li>Issues in foregin repos: {{issues|length}}</li>
+          <li>Stars: {{stars|length}}</li>
+        </ul>
+      </div>
+      <div class="mdl-card__actions mdl-card--border">
+        <small><b>Generated:</b> {{timestamp}}</small>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
